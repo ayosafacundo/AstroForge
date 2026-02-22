ac0d0f42a13eeb3233237725ac59edb62af35520
Facundo Ezequiel Ayosa <ayosafacundo@gmail.com>
2026-02-22 15:02:41 -0300

    Linux capable frontend, added README.md and Design_Principles.MD

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
-
+import { env } from "process";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "localhost",
-    port: 5173,
+    host: env.HOST,
+    port: parseInt(env.PORT ? env.PORT : "5173"),
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
